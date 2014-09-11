18cf4fa2fb17323a868f8aeb9463939cbbf09893
Jairo Suarez <jasuarezpr@gmail.com>
2014-09-11 05:42:30 -0500

    added gui

---
+++ src/unalcol/agents/examples/labyrinth/teseo/cualquierCosa/Position.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package unalcol.agents.examples.labyrinth.teseo.cualquierCosa;
+
+import java.awt.Point;
+
+/**
+ *
+ * @author Jairo
+ */
+public class Position extends Point {
+    
+}
