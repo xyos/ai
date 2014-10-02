110fd16b4efd86bf8669550c66c374d6d910e543
Jairo Suarez <jasuarezpr@gmail.com>
2014-10-01 21:50:34 -0500

    adding a no_op for Agent Front

--- src/unalcol/agents/examples/labyrinth/teseo/cualquierCosa/SimpleTeseoAgentProgram.java
+++ src/unalcol/agents/examples/labyrinth/teseo/cualquierCosa/SimpleTeseoAgentProgram.java
@@ -45,7 +45,7 @@ public abstract class SimpleTeseoAgentProgram  implements AgentProgram{
     cmd.clear();
   }
 
-  public abstract int accion( boolean PF, boolean PD, boolean PA, boolean PI, boolean MT,
+    public abstract int accion( boolean PF, boolean PD, boolean PA, boolean PI, boolean MT,
           boolean AF, boolean AD, boolean AA, boolean AI);
 
   /**
@@ -54,10 +54,8 @@ public abstract class SimpleTeseoAgentProgram  implements AgentProgram{
    * @param perception Perception
    * @return Action[]
    */
-  public Action compute(Percept p){
-    System.gc();
-    if( cmd.size() == 0 ){
-
+    public Action compute(Percept p) {
+        System.gc();
         boolean PF = (Boolean) p.getAttribute(language.getPercept(0));
         boolean PD = (Boolean) p.getAttribute(language.getPercept(1));
         boolean PA = (Boolean) p.getAttribute(language.getPercept(2));
@@ -67,53 +65,60 @@ public abstract class SimpleTeseoAgentProgram  implements AgentProgram{
         boolean AD = (Boolean) p.getAttribute(language.getPercept(6));
         boolean AA = (Boolean) p.getAttribute(language.getPercept(7));
         boolean AI = (Boolean) p.getAttribute(language.getPercept(8));
+        if (cmd.size() == 0) {
 
-      
       //System.out.println("---------------\nPocisión: "+actualNode.getX()+","+actualNode.getY()+"\nBrújula: "+norte);
-      //System.out.println("ToNorth:"+actualNode.exploredStates[0]+", ToEast:"+actualNode.exploredStates[1]+", ToSouth:"+actualNode.exploredStates[2]+", ToWest:"+actualNode.exploredStates[3]);
-      //System.out.println("Graph: "); printNodes();
-      //System.out.println("---------------");
-      //new java.util.Scanner(System.in).nextLine(); //Sirve para ver el proceso paso paso.
-      int d = accion(PF, PD, PA, PI, MT, AF, AD, AA, AI);
-      if (0 <= d && d < 4) {
-        for (int i = 1; i <= d; i++) {
-          cmd.add(language.getAction(3)); //rotate
-          rotar(1);
+            //System.out.println("ToNorth:"+actualNode.exploredStates[0]+", ToEast:"+actualNode.exploredStates[1]+", ToSouth:"+actualNode.exploredStates[2]+", ToWest:"+actualNode.exploredStates[3]);
+            //System.out.println("Graph: "); printNodes();
+            //System.out.println("---------------");
+            //new java.util.Scanner(System.in).nextLine(); //Sirve para ver el proceso paso paso.
+            int d = accion(PF, PD, PA, PI, MT, AF, AD, AA, AI);
+            if (0 <= d && d < 4) {
+                for (int i = 1; i <= d; i++) {
+                    cmd.add(language.getAction(3)); //rotate
+                    rotar(1);
+                }
+                cmd.add(language.getAction(2)); // advance
+
+                GraphNode newNode = nextMove();
+                if (!knownNode(newNode)) {
+                    actualNode.addNeighbor(newNode, 1);
+                    actualNode = newNode;
+                    myGraph.addNode(actualNode);
+                    isNewNode = true;
+                } else {
+                    actualNode = myGraph.SearchNode(actualNode.getX(), actualNode.getY());
+                    isNewNode = false;
+                }
+                myGraph.reduceGraph(this.TwoWallsNodes);
+            } else {
+                if (d == 5) {
+                    myGraph.reduceGraph(this.TwoWallsNodes);
+                    goBackDecisionNode(actualNode);
+                }
+                if (d == -1) {
+                    cmd.add(language.getAction(1)); // die
+                }
+            }
         }
-        cmd.add(language.getAction(2)); // advance
-        
-        GraphNode newNode = nextMove();
-        if(!knownNode(newNode)){
-            actualNode.addNeighbor(newNode, 1);
-            actualNode = newNode;
-            myGraph.addNode(actualNode);
-            isNewNode=true;
-        }else{
-            actualNode = myGraph.SearchNode(actualNode.getX(), actualNode.getY());
-            isNewNode=false;
+        String x = cmd.get(0);
+        if (x.equals(language.getAction(1))) {
+            System.out.println("El agente ha muerto\n--------------------");
+            this.stop = System.currentTimeMillis();
+            System.out.println("| Número de nodos:" + myGraph.getNodes().size() + " | Tiempo: " + (this.stop - this.myGraph.start) / 1000 + " segundos |");
+            System.out.println("---------------------------------------------");
+            printNodes();
+            new java.util.Scanner(System.in).nextLine();
+        } else if (x.equals(language.getAction(2))) {
+            if (AF) {
+                System.out.println("agente al frente");
+                x = language.getAction(0);
+                cmd.add(x);
+            }
         }
-        myGraph.reduceGraph(this.TwoWallsNodes);
-      }
-      else{
-          if(d==5){
-              myGraph.reduceGraph(this.TwoWallsNodes);
-              goBackDecisionNode(actualNode);
-          }
-          if(d==-1) cmd.add(language.getAction(0)); // die
-      }
+        cmd.remove(0);
+        return new Action(x);
     }
-    String x = cmd.get(0);
-    if(cmd.get(0).equals(language.getAction(0))){
-        System.out.println("El agente ha muerto\n--------------------");
-        this.stop=System.currentTimeMillis();
-        System.out.println("| Número de nodos:"+myGraph.getNodes().size()+" | Tiempo: "+(this.stop-this.myGraph.start)/1000+" segundos |"); 
-        System.out.println("---------------------------------------------");
-        printNodes();
-        new java.util.Scanner(System.in).nextLine();
-    }
-    cmd.remove(0);
-    return new Action(x);
-  }
 
   /**
    * goalAchieved
